9885dfb9c9804c4606b5297fef15a19e3824f025
Nadia Tabassum <shormy.992@gmail.com>
2022-12-13 13:28:06 +0600

    Added Navbar, Header, Footer, Layout and showed product Data in Home Component(index.js). Also added relevant stylings and image assets.

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import allProducts from './data/products.json';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Plants | Home</title>
+      </Head>
+      <div className="container">
+        <h2 className={styles.title}>
+          All Products <span>🌿</span>
+        </h2>
+        <div className={styles.products_container}>
+          {allProducts.map((product) => {
+            return (
+              <div className={styles.product_card} key={product.id}>
+                <Link legacyBehavior href={`products/${product.slug}`}>
+                  <a>
+                    <div className={styles.product_img}>
+                      <img src={product.image.url} alt={product.name} />
+                    </div>
+                  </a>
+                </Link>
+                <div className={styles.product_content}>
+                  <h3>{product.name}</h3>
+                  <p>${product.price}</p>
+                  <button className="btn">Add to cart 🛒</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
